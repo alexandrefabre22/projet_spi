a8f887f33a11ccd3bf5e0e42d53819323b672176
Kergann LE CORNEC <kergann.le_cornec@uca.fr>
2019-06-04 15:24:45 +0200

    Comment with doxygene

--- lib/student.cpp
+++ lib/student.cpp
@@ -1,3 +1,7 @@
+/**
+ *@file student.cpp
+ */
+
 #include "student.h"
 
 Student::Student(std::string name, bool present): m_name(name), m_present(present)
@@ -5,25 +9,46 @@ Student::Student(std::string name, bool present): m_name(name), m_present(presen
 
 }
 
+
+/**
+ * This methods returns the name of a student
+ */
+
 std::string Student::name() const
 {
     return m_name;
 }
+
+/**
+ * This methods taks a string to set the name of a student
+ */
 
 void Student::setName(const std::string &name)
 {
     m_name = name;
 }
 
+
+/**
+ * This methods returns if a student is presents or not
+ */
+
 bool Student::present() const
 {
     return m_present;
 }
 
+/**
+ * This methods sets with a booleen ,the presence of a student.
+ */
 void Student::setPresent(bool present)
 {
     m_present = present;
 }
+
+/**
+ * This methods prints the name of the  present student.
+ */
 
 std::string Student::print() const
 {
@@ -31,6 +56,9 @@ std::string Student::print() const
     return out;
 }
 
+/**
+ * This methods counts the number of present student.
+ */
 int Student::nb_presents(const std::vector<Student> &students)
 {
     int n = 0;
